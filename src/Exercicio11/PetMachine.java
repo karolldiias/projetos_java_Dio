0b728ba306d5890a43d1f25fd382bc67624116a1
karolldiias <karolldiias@gmail.com>
2025-11-11 15:09:02 -0300

    feat: Resolução do exercicio 11

--- src/Exercicio11/PetMachine.java
+++ src/Exercicio11/PetMachine.java
@@ -2,22 +2,86 @@ package Exercicio11;
 
 public class PetMachine {
 
-  private boolean clean;
+  private boolean clean = true;
 
-  private int water;
+  private int water = 30;
 
-  private int shampoo;
+  private int shampoo = 10;
 
   private Pet pet;
 
 
-  public void takeAShower(){
-    if(this.pet == null){
+  // Método dar banho no pet
+  public void takeAShower() {
+    if (this.pet == null) {
       System.out.println("Coloque o pet na máquina para inicial o banho");
       return;
     }
 
+    this.water -= 10;
+    this.shampoo -= 2;
     pet.setClean(true);
     System.out.println("O pet" + pet.getName() + "está limpo");
   }
+
+  // Método adicionar água na máquina
+  public void addWater() {
+    if (water == 30) {
+      System.out.println("A capacidade de água da máquina esta no máximo");
+      return;
+    }
+    water += 2;
+  }
+
+  // Método adicionar Shampoo na máquina
+  public void addShampoo() {
+    if (shampoo == 10) {
+      System.out.println("A capacidade de shampoo da máquina esta no máximo");
+      return;
+    }
+    shampoo += 2;
+  }
+
+  public int getWater() {
+    return water;
+  }
+
+  public int getShampoo() {
+    return shampoo;
+  }
+
+  public boolean hasPet() {
+    return pet != null;
+  }
+
+  public void setPet(Pet pet) {
+    if (this.clean) {
+      System.out.println("A máquina esta suja, para colocar o pet é necessário limpá-la");
+      return;
+    }
+
+    if (hasPet()) {
+      System.out.println("O pet" + this.pet.getName() + "está na máquina nesse momento");
+      return;
+    }
+    
+    this.pet = pet;
+    System.out.println("O pet" + pet.getName() + "foi colocado na máquina");
+  }
+
+  // método remover pet da máquina
+  public void removePet() {
+    this.clean = this.pet.isClean();
+    System.out.println("O pet" + this.pet.getName() + "foi retirado da máquina");
+    this.pet = null;
+  }
+
+  // método limpar máquina
+  public void wash() {
+    this.water -= 10;
+    this.shampoo -= 2;
+    this.clean = true;
+    System.out.println("A máquina foi limpa");
+  }
+
 }
