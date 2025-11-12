767cc6be9d13c2c9500ed58f95a345ae52e02a56
karolldiias <karolldiias@gmail.com>
2025-11-12 12:58:33 -0300

    feat: resolução exercicio 14

---
+++ src/Exercicio14/BRClock.java
@@ -0,0 +1,16 @@
+package Exercicio14;
+
+public non-sealed class BRClock extends Clock {
+
+  @Override
+  public Clock convert(Clock clock) {
+    this.second = clock.getSecond();
+    this.minute = clock.getMinute();
+    switch (clock) {
+      case USClock usClock -> this.hour = (usClock.getPeriodIndicator().equals("PM")) ? usClock.getHour() + 12 : usClock.getHour();
+      case BRClock brClock -> this.hour = brClock.getHour();
+    }
+    return this;
+  }
+
+}
