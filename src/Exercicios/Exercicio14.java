24bd8da850eb6cf19748d53044712baba999b4f8
karolldiias <karolldiias@gmail.com>
2025-11-12 11:41:25 -0300

    feat: Adição dos exercicios 12, 13 e 14

---
+++ src/Exercicios/Exercicio14.java
@@ -0,0 +1,17 @@
+package Exercicios;
+
+public class Exercicio14 {
+  /*
+   * Escreve uma hierarquia de classes para criação de relógios para diferentes
+   * lugares do mundo, defina uma classe pai que tenha as propriedades Hora,
+   * minuto e segundo com seus respectivos getters e setters (atendendo as regras
+   * do funcionamento de um relógio) e um método que deverá retornar a hora no
+   * formato HH:MM:SS, a partir dessa classe crie 2 implementações, uma chamada
+   * relógio Americano e outra chamada relógio Brasileiro, lembrando que para o
+   * relógio americano não existem as horas de 13 até 24. Defina também na super
+   * classe um método que ficará por responsabilidade da classe que extender-la
+   * definir seu funcionamento, esse método deve receber um relógio (
+   * independente da implementação) e deve-se extrair as informações dele e usa-la
+   * no objeto que recebeu para setar as novas informações do relógio.
+   */
+}
