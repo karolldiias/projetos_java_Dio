4c4157e78b4d658ff47adc2c13cd25550bc46c16
karolldiias <karolldiias@gmail.com>
2025-11-13 12:14:30 -0300

    feat: Adição do desafio 4

---
+++ src/Exercicios/Desafio4.java
@@ -0,0 +1,26 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Desafio4 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    //João Pedro de Albuquerque Magalhães
+    System.out.println("Qual o seu nome: ");
+    String nomeCompleto = sc.nextLine();
+
+    if(nomeCompleto.length() <= 30){
+      System.out.println("Entrada válida" + nomeCompleto.length());
+    } else {
+      System.out.println("Nome muito longo" + nomeCompleto.length());
+    }
+
+
+    // TODO: Implemente a estrutura condicional para verifica o tamanho do texto
+    // digitado:
+
+    sc.close();
+  }
+}
