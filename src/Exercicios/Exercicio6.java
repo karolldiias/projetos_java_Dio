76bdc9e4c316229eb56834e322eebcdb630ca706
karolldiias <karolldiias@gmail.com>
2025-11-14 17:42:01 -0300

    feat:Adição da solução do exercicio6

--- src/Exercicios/Exercicio6.java
+++ src/Exercicios/Exercicio6.java
@@ -1,9 +1,27 @@
 package Exercicios;
 
+import java.util.Scanner;
+
 /*
  * Escreva um código que receba o tamanho do lado de um quadrado, calcule sua área e exiba na tela
   fórmula: área=lado X lado
  */
 public class Exercicio6 {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    //Entrada lado de um quadrado
+    System.out.println("Digite o tamanho do lado do quadrado para saber sua área:");
+    double ladoQuadrado = scanner.nextDouble();
+
+    //Calculo da área
+    double totalArea = (ladoQuadrado*ladoQuadrado);
+    
+    //Exibir resultado
+    System.out.printf("A área do quadrado é de %.2f.", totalArea);
+
+    scanner.close();
+  }
   
 }
