620522950b1b75d2b0ef908ceb11fef0c2409600
karolldiias <karolldiias@gmail.com>
2025-11-14 17:59:56 -0300

    feat: Adição resolução exercicio 8

--- src/Exercicios/Exercicio7.java
+++ src/Exercicios/Exercicio7.java
@@ -1,9 +1,29 @@
 package Exercicios;
 
+import java.util.Scanner;
+
 /*
  * Escreva um código que receba a base e a altura de um retângulo, calcule sua área e exiba na tela
   fórmula: área=base X altura 
  */
 public class Exercicio7 {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    //Entrada base do retângulo
+    System.out.println("Digite o valor da base do retângulo");
+    double base = scanner.nextDouble();
+
+    // Entrada altura do retângulo
+    System.out.println("Digite o valor da altura do retângulo");
+    double altura = scanner.nextDouble();
+
+    //Cálculo da área do retângulo
+    double area = base * altura;
+
+    //Exibir mensagem
+    System.out.printf("A área do retângulo é %.2f", area);
+  }
   
 }
--- src/Exercicios/Exercicio8.java
+++ src/Exercicios/Exercicio8.java
@@ -1,6 +1,29 @@
 package Exercicios;
 
+import java.util.Scanner;
+
 // Escreva um código que receba o nome e a idade de 2 pessoas e imprima a diferença de idade entre elas
 public class Exercicio8 {
-  
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    // Entrada nome e idade da primeira pessoa
+    System.out.println("Digite o nome da primeira pessoa:");
+    String nomePrimeiro = scanner.next();
+    System.out.println("Olá, " + nomePrimeiro +", digite sua idade:");
+    int idadePrimeiro = scanner.nextInt();
+
+    // Entrada nome e idade da segunda pessoa
+    System.out.println("Digite o nome da segunda pessoa:");
+    String nomeSegundo = scanner.next();
+    System.out.println("Olá, " + nomeSegundo +", digite sua idade:");
+    int idadeSegundo = scanner.nextInt();
+
+    // Cálculo da diferença das idades - Math.abs garante o valor positivo independente da ordem das idades
+    int diferencaIdade = Math.abs(idadePrimeiro - idadeSegundo);
+
+    //Exibir mensagem
+    System.out.printf("A diferença de idade entre %s e %s é de %s anos", nomePrimeiro, nomeSegundo, diferencaIdade);
+
+  }
 }
